8d96ff7d3e546ea86fc851ab7572c2d02c9396fc
jmkiley <jordankiley@gmail.com>
2016-07-06 16:51:12 -0400

    Done.

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -26,37 +26,42 @@ let b = 32
 */
 // write your code here
 
+print(a + b)
 
 /*: Question 2
 ### 2. Print the result of a modulo b is equal to zero
 */
 // write your code here
 
+print(a % b == 0)
 
 /*: Question 3
 ### 3. Print the result of y times b less than or equal to a
 */
 // write your code here
 
+print(y * b <= a)
 
 /*: Question 4
 ### 4. Print the inverse of a greater than or equal to b
 */
 // write your code here
 
+print(!(a>=b))
 
 
 /*: Question 5
 ### 5. Print "true" if a modulo b is equal to zero
 */
 // write your code here
-
+print(a % b == 0)
 
 /*: Question 6
 ### 6. Print "true" if a divided by b is greater than x
 */
 // write your code here
 
+print(a / b > Int(x))
 
 
 /*: Question 7
@@ -64,7 +69,8 @@ let b = 32
 */
 // write your code here
 
-
+var xInt:Int = Int (x)
+print( y / xInt > 3)
 
 
 /*: Question 8
@@ -72,7 +78,7 @@ let b = 32
 */
 // write your code here
 
-
+print(y > Int(x) && a / b > 9)
 
 
 /*: Question 9
@@ -81,23 +87,40 @@ let b = 32
 // write your code here
 
 
+func isGreater(first first : Int, second : Int) -> Bool {
+    return (first > second)
+}
 
-
+isGreater(first: 6, second: 3)
 
 
 /*: Question 10
 ### 10. write a function "isForceWith" that takes a String argument and returns true if the argument is the name of some whom the force is strong with, otherwise it returns false.  People who have the force are Luke, Leia, Anakin, Obi Wan, Yoda, Vader.
 */
 // write your code here
+func isForceWith(person: String) -> Bool {
+    if (person == "Luke" || person == "Leia" || person == "Anakin" || person == "Obi Wan" || person == "Yoda" || person == "Vader") {
+        return true
+    } else {
+        return false
+    }
+}
 
-
-
+isForceWith("Luke")
 /*: Question 11
 ### 11. Create a function where the two arguments represent different bank account (one of those bank accounts is yours). What type should these arguments be if we are to then perform some math operations on them? Setup a conditional that will add 10 to your funds and minus 10 from the other funds (bank account) if the other account won't go negative if we were to take 10 dollars from it.
 */
 // write your code here
 
-
+func compareBankAccounts(myAccount myAccount : Int, theirAccount : Int) {
+    var theirAccount = theirAccount
+    var myAccount = myAccount
+    if (theirAccount > 10) {
+        theirAccount -= 10
+        myAccount += 10
+    }
+    
+}
 
 
 
